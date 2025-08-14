237e08e6ed119c079c30bd47b85c7bbfaf12591e
Your Name <gormelkonyan005@gmail.com>
2025-08-14 19:27:15 +0400

    change wheit indicator ui

--- lib/widgets/wheit_indicator.dart
+++ lib/widgets/wheit_indicator.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_api/themes/themes.dart';
 
-class WheitIndicator extends StatelessWidget {
+class WheitIndicator extends StatefulWidget {
   const WheitIndicator({super.key});
+
+
+  @override
+  State<WheitIndicator> createState() => _WheitIndicatorState();
+}
+
+class _WheitIndicatorState extends State<WheitIndicator> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 5), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Themes().themeData;
     return Center(
-      child: CircularProgressIndicator(color: theme.tabBarTheme.indicatorColor),
+      child: _isLoading
+          ? CircularProgressIndicator(color: theme.tabBarTheme.indicatorColor)
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.bar_chart_rounded,
+                  color: theme.primaryColor,
+                  size: 80,
+                ),
+                Text(
+                  'Try to get crypto coins!',
+                  style: theme.textTheme.bodyMedium,
+                ),
+              ],
+            ),
     );
   }
 }
